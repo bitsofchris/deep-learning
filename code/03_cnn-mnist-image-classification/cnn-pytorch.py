5b064723e878fb6948cdf2b7a38c777f4ab017cf
bitsofchris <bitsofchris01@gmail.com>
2024-11-07 06:55:35 -0500

    cnn architecture

--- code/03_cnn-mnist-image-classification/cnn-pytorch.py
+++ code/03_cnn-mnist-image-classification/cnn-pytorch.py
@@ -39,20 +39,40 @@ class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv_stack = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3),  # 1 input channel, 32 output channels
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3),  # 32 input channels, 64 output channels
+            # initial in_channels are the number of color channels in our input data
+            # grayscale = 1, RGB = 3. Our data using 1
+            # number of feature maps = out_channels
+            # kernel_size is the size of the local receptive field as a  square (5x5)
+            nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5),  # 1 input channel, 32 output channels
+            nn.ReLU(), # applies Rectified Linear Unit activation function to output of convlutional layer
+            nn.MaxPool2d(kernel_size=2),  # Max pooling layer, downsamples the feature maps reducing their size
+            nn.Conv2d(in_channels=20, out_channels=40, kernel_size=5),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),  # Max pooling layer
+            # Flatten takes a tensor as input and returns a 1D tensor of the same size
+            # takes tensor of (batch_size, channels, height, width) and returns tensor of (batch_size, channels * height * width)
             nn.Flatten(),
-            nn.Linear(64 * 12 * 12, 128),  # 64 * 12 * 12 input neurons
+            # 40 feature maps * the 4x4 size of each feature map
+            # the 28x28 image becaomes a 24x24 feature map after the first convolutional layer because of the 5x5 window
+            # the 24x24 feature map becomes a 12x12 feature map after the first max pooling layer (2x2 window)
+            # the 12x12 feature map becomes a 8x8 feature map after the second convolutional layer because of the 5x5 window
+            # the 8x8 feature map becomes a 4x4 feature map after the second max pooling layer (2x2 window)
+            # input to the fully connected layer is the flattened tensor of size 40 * 4 * 4
+            nn.Linear(in_features=40 * 4 * 4, out_features=100),  # 64 * 12 * 12 input neurons
             nn.ReLU(),
-            nn.Linear(128, 10),  # 10 output neurons
+            nn.Linear(in_features=100, out_features=100),
+            nn.ReLU(),
+            nn.Linear(in_features=100, out_features=10),
+            nn.Softmax(dim=1) # applies softmax function to the output neurons
         )
 
     def forward(self, x):
-        logits = self.conv_stack(x)
-        return logits
+        for layer in self.conv_stack:
+            x = layer(x)
+            print(f"{layer.__class__.__name__} output shape: {x.shape}")
+        return x
+        # logits = self.conv_stack(x)
+        # return logits
 
 
 model = ConvolutionalNeuralNetwork().to(device)
